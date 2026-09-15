18f728d07df11016320c0a1bddce9bec4096b170
ski-sketch <mustafamoamen980@gmail.com>
2026-09-16 02:22:11 +0400

    Added code to improvedpacksniff.py

---
+++ improvedpacksniff.py
@@ -0,0 +1,10 @@
+from scapy.all import sniff
+from scapy.layers.inet import IP, TCP, UDP, Ether
+packet = sniff(count=1)
+packet.show()
+print(packet[0].layers())
+if packet[0].haslayer(IP):
+    print(packet[0].show())
+print(len(packet[0]))
+print(packet[0][IP].len)
+print(packet[0][TCP].dataofs * 4)
