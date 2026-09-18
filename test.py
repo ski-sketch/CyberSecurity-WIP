502fc27fbe72c9594f66a40ab7373e792012b117
ski-sketch <mustafamoamen980@gmail.com>
2026-09-19 02:43:04 +0400

    Made my analyzer more readable and overall effecient

--- test.py
+++ test.py
@@ -1,72 +1,108 @@
 from scapy.all import sniff
-from scapy.layers.inet import IP,TCP,UDP,ICMP
+from scapy.layers.inet import IP, TCP, UDP, ICMP
 from scapy.layers.inet6 import IPv6, ICMPv6EchoRequest, ICMPv6EchoReply
 from scapy.layers.l2 import Ether, ARP
-packet = sniff(timeout=2)
-lenpacket = len(packet)
-for i in range(lenpacket):
-    if packet[i].haslayer(Ether):
-        if packet[i].haslayer(IP):
-            if packet[i].haslayer(TCP):
-                print ("TCP Packet")
-                print("Source Port: " + str(packet[i][TCP].sport) +
-                      "\nSource IP: " + str(packet[i][IP].src) +
-                      "\nDestination Port: " + str(packet[i][TCP].dport) +
-                      "\nDestination IP: " + str(packet[i][IP].dst) +
-                      "\nSequence Number: " + str(packet[i][TCP].seq) +
-                      "\nAcknowledgment Number: " + str(packet[i][TCP].ack) +
-                      "\nFlags: " + str(packet[i][TCP].flags))
-            elif packet[i].haslayer(UDP):
-                print ("UDP Packet")
-                print("Source Port: " + str(packet[i][UDP].sport) +
-                      "\nDestination Port: " + str(packet[i][UDP].dport) +
-                      "\nLength: " + str(packet[i][UDP].len) +
-                      "\nChecksum: " + str(packet[i][UDP].chksum))
-            elif packet[i].haslayer(ICMP):
-                print ("ICMP Packet")
-                print("Type: " + str(packet[i][ICMP].type) +
-                      "\nCode: " + str(packet[i][ICMP].code) +
-                      "\nChecksum: " + str(packet[i][ICMP].chksum) +
-                      "\nID: " + str(packet[i][ICMP].id) +
-                      "\nSequence: " + str(packet[i][ICMP].seq))
-        if packet[i].haslayer(IPv6):
-            if packet[i].haslayer(TCP):
-                print ("TCP Packet")
-                print("Source Port: " + str(packet[i][TCP].sport) +
-                      "\nSource IP: " + str(packet[i][IPv6].src) +
-                      "\nDestination Port: " + str(packet[i][TCP].dport) +
-                      "\nDestination IP: " + str(packet[i][IPv6].dst) +
-                      "\nSequence Number: " + str(packet[i][TCP].seq) +
-                      "\nAcknowledgment Number: " + str(packet[i][TCP].ack) +
-                      "\nFlags: " + str(packet[i][TCP].flags))
-            elif packet[i].haslayer(UDP):
-                print ("UDP Packet")
-                print("Source Port: " + str(packet[i][UDP].sport) +
-                      "\nDestination Port: " + str(packet[i][UDP].dport) +
-                      "\nLength: " + str(packet[i][UDP].len) +
-                      "\nChecksum: " + str(packet[i][UDP].chksum))
-            elif packet[i].haslayer(ICMPv6EchoRequest):
-                print ("ICMPv6 Echo Request Packet")
-                print("Type: " + str(packet[i][ICMPv6EchoRequest].type) +
-                      "\nCode: " + str(packet[i][ICMPv6EchoRequest].code) +
-                      "\nChecksum: " + str(packet[i][ICMPv6EchoRequest].cksum) +
-                      "\nID: " + str(packet[i][ICMPv6EchoRequest].id) +
-                      "\nSequence: " + str(packet[i][ICMPv6EchoRequest].seq))
-            elif packet[i].haslayer(ICMPv6EchoReply):
-                print ("ICMPv6 Echo Reply Packet")
-                print("Type: " + str(packet[i][ICMPv6EchoReply].type) +
-                      "\nCode: " + str(packet[i][ICMPv6EchoReply].code) +
-                      "\nChecksum: " + str(packet[i][ICMPv6EchoReply].cksum) +
-                      "\nID: " + str(packet[i][ICMPv6EchoReply].id) +
-                      "\nSequence: " + str(packet[i][ICMPv6EchoReply].seq))
-    if packet[i].haslayer(ARP):
-        print ("ARP Packet")
-        print("Hardware Type: " + str(packet[i][ARP].hwtype) +
-              "\nProtocol Type: " + str(packet[i][ARP].ptype) +
-              "\nHardware Size: " + str(packet[i][ARP].hwlen) +
-              "\nProtocol Size: " + str(packet[i][ARP].plen) +
-              "\nOpcode: " + str(packet[i][ARP].op) +
-              "\nSource MAC: " + str(packet[i][ARP].hwsrc) +
-              "\nSource IP: " + str(packet[i][ARP].psrc) +
-              "\nDestination MAC: " + str(packet[i][ARP].hwdst) +
-              "\nDestination IP: " + str(packet[i][ARP].pdst))
+
+
+def analyze_packet(packet):
+
+
+    if packet.haslayer(Ether):
+        print("\n========== ETHERNET ==========")
+        print("Source MAC: " + str(packet[Ether].src))
+        print("Destination MAC: " + str(packet[Ether].dst))
+
+
+    if packet.haslayer(IP):
+
+        print("\n========== IPv4 ==========")
+        print("Source IP: " + str(packet[IP].src))
+        print("Destination IP: " + str(packet[IP].dst))
+        print("TTL: " + str(packet[IP].ttl))
+
+
+        if packet.haslayer(TCP):
+            print("\n---------- TCP ----------")
+            print("Source Port: " + str(packet[TCP].sport))
+            print("Destination Port: " + str(packet[TCP].dport))
+            print("Sequence Number: " + str(packet[TCP].seq))
+            print("Acknowledgment Number: " + str(packet[TCP].ack))
+            print("Flags: " + str(packet[TCP].flags))
+
+
+        elif packet.haslayer(UDP):
+            print("\n---------- UDP ----------")
+            print("Source Port: " + str(packet[UDP].sport))
+            print("Destination Port: " + str(packet[UDP].dport))
+            print("Length: " + str(packet[UDP].len))
+            print("Checksum: " + str(packet[UDP].chksum))
+
+
+        elif packet.haslayer(ICMP):
+            print("\n---------- ICMP ----------")
+            print("Type: " + str(packet[ICMP].type))
+            print("Code: " + str(packet[ICMP].code))
+            print("Checksum: " + str(packet[ICMP].chksum))
+            print("ID: " + str(packet[ICMP].id))
+            print("Sequence: " + str(packet[ICMP].seq))
+
+
+    if packet.haslayer(IPv6):
+
+        print("\n========== IPv6 ==========")
+        print("Source IP: " + str(packet[IPv6].src))
+        print("Destination IP: " + str(packet[IPv6].dst))
+        print("Hop Limit: " + str(packet[IPv6].hlim))
+
+
+        if packet.haslayer(TCP):
+            print("\n---------- TCP ----------")
+            print("Source Port: " + str(packet[TCP].sport))
+            print("Destination Port: " + str(packet[TCP].dport))
+            print("Sequence Number: " + str(packet[TCP].seq))
+            print("Acknowledgment Number: " + str(packet[TCP].ack))
+            print("Flags: " + str(packet[TCP].flags))
+
+
+        elif packet.haslayer(UDP):
+            print("\n---------- UDP ----------")
+            print("Source Port: " + str(packet[UDP].sport))
+            print("Destination Port: " + str(packet[UDP].dport))
+            print("Length: " + str(packet[UDP].len))
+            print("Checksum: " + str(packet[UDP].chksum))
+
+
+        elif packet.haslayer(ICMPv6EchoRequest):
+            print("\n---------- ICMPv6 Echo Request ----------")
+            print("Type: " + str(packet[ICMPv6EchoRequest].type))
+            print("Code: " + str(packet[ICMPv6EchoRequest].code))
+            print("Checksum: " + str(packet[ICMPv6EchoRequest].cksum))
+            print("ID: " + str(packet[ICMPv6EchoRequest].id))
+            print("Sequence: " + str(packet[ICMPv6EchoRequest].seq))
+
+
+        elif packet.haslayer(ICMPv6EchoReply):
+            print("\n---------- ICMPv6 Echo Reply ----------")
+            print("Type: " + str(packet[ICMPv6EchoReply].type))
+            print("Code: " + str(packet[ICMPv6EchoReply].code))
+            print("Checksum: " + str(packet[ICMPv6EchoReply].cksum))
+            print("ID: " + str(packet[ICMPv6EchoReply].id))
+            print("Sequence: " + str(packet[ICMPv6EchoReply].seq))
+
+
+    if packet.haslayer(ARP):
+
+        print("\n========== ARP ==========")
+        print("Hardware Type: " + str(packet[ARP].hwtype))
+        print("Protocol Type: " + str(packet[ARP].ptype))
+        print("Hardware Size: " + str(packet[ARP].hwlen))
+        print("Protocol Size: " + str(packet[ARP].plen))
+        print("Opcode: " + str(packet[ARP].op))
+        print("Source MAC: " + str(packet[ARP].hwsrc))
+        print("Source IP: " + str(packet[ARP].psrc))
+        print("Destination MAC: " + str(packet[ARP].hwdst))
+        print("Destination IP: " + str(packet[ARP].pdst))
+
+
+
+sniff(prn=analyze_packet, timeout=10)
